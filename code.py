094adb592a1a0056bbe447de5af27f3a4e7c9e08
lambtor <lambtor@yahoo.com>
2022-03-30 11:47:26 -0500

    Update code.py
    i2s audio really helps sound quality

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 import board
 import digitalio
 import neopixel
-import audioio
+# import audioio
 import audiocore
 # from audiocore import WaveFile
 from adafruit_debouncer import Debouncer
@@ -9,7 +9,7 @@ import time
 
 # this script is meant to be used with qt py rp2040
 # this is for stemma speaker board. drop wav file on main directory of board
-# from adafruit_circuitplayground import cp
+import audiobusio
 # OK OMFG circuitpython has a max of 88 characters per line of code
 # -------------------------------------------------------------------------------------
 
@@ -17,8 +17,8 @@ import time
 # to behave for your hardware
 # true or false here. False means program is for a type 2
 IS_TYPE_ONE_PHASER = False
-SETTING_LED_PIN = board.A2
-BEAM_LED_PIN = board.A1
+SETTING_LED_PIN = board.A1
+BEAM_LED_PIN = board.A2
 # if using stemma speaker board, this MIGHT use pin SDA1?
 # only 3 pins connected when using stemma speaker board, so maybez
 SOUND_OUT_PIN = board.A0
@@ -26,6 +26,7 @@ SETTING_SND_FILE = "adjust.wav"
 BTN_LEFT = board.TX
 BTN_RIGHT = board.RX
 BTN_TRIGGER = board.BUTTON
+# BUTTON
 
 # --------
 mnIntensitySetting = 0
@@ -35,7 +36,17 @@ if IS_TYPE_ONE_PHASER:
 else:
     mnSettingLEDMax = 17
 mnBeamLEDCount = 1
-moAudioPlay = audioio.AudioOut(SOUND_OUT_PIN)
+
+# try:
+#    from audioio import AudioOut
+# except ImportError:
+#    try:
+#        from audiopwmio import PWMAudioOut as AudioOut
+#    except ImportError:
+#        pass
+moI2SAudio = audiobusio.I2SOut(board.SDA, board.SCL, board.SCK)
+
+# moAudioPlay = AudioOut(SOUND_OUT_PIN)
 moSettingSoundFile = open(SETTING_SND_FILE, "rb")
 moSettingSound = audiocore.WaveFile(moSettingSoundFile)
 
@@ -58,11 +69,9 @@ mpinBoardLed.show()
 
 # 6-8 status leds in a single row, or 16 split across 2 rows
 # setting lowest value is 0, max is count of setting LEDs?
-moPixelRow = neopixel.NeoPixel(SETTING_LED_PIN, mnSettingLEDMax - 1)
-moPixelRow.brightness = 0.3
-moPixelRow.auto_write = False
-moPixelRow.fill(moRGBBlack)
-moPixelRow.show()
+moSettingRow = neopixel.NeoPixel(SETTING_LED_PIN, mnSettingLEDMax - 1)
+moSettingRow.brightness = 0.1
+moSettingRow.auto_write = False
 
 # a laser pointer driven by 5v, with 1 neopixel on each side.
 # these are going to be mounted on a custom pcb with a hole to fit pointer
@@ -103,14 +112,14 @@ def SettingIncrease(nAmount):
 
 def UpdateSetting():
     global mnIntensitySetting
-    global moPixelRow
+    global moSettingRow
     global moRGBRed
     global moRGBBlack
     global moRGBStrength
     global mnSettingLEDMax
     if mnIntensitySetting == 0:
-        # moPixelRow.fill(moRGBBlack)
-        # moPixelRow.show()
+        # moSettingRow.fill(moRGBBlack)
+        # moSettingRow.show()
         WarningShotMode()
         return
 
@@ -121,31 +130,31 @@ def UpdateSetting():
         if nIterator > 7:
             if mnIntensitySetting >= 8:
                 if mnIntensitySetting > nIterator:
-                    moPixelRow[nIterator] = moRGBRed
+                    moSettingRow[nIterator] = moRGBRed
                 else:
-                    moPixelRow[nIterator] = moRGBBlack
+                    moSettingRow[nIterator] = moRGBBlack
         else:
             if mnIntensitySetting > 8:
                 # if mnIntensitySetting >= nIterator:
                 nRed = int((mnIntensitySetting - 7) * 16)
                 nGreen = int(moRGBStrength - (8 * (mnIntensitySetting - 7)))
-                moPixelRow[nIterator] = (nRed, nGreen, 0)
+                moSettingRow[nIterator] = (nRed, nGreen, 0)
             else:
                 if nIterator < mnIntensitySetting:
                     # set any LED at or below current setting as green
-                    moPixelRow[nIterator] = (0, moRGBStrength, 0)
+                    moSettingRow[nIterator] = (0, moRGBStrength, 0)
                 else:
-                    moPixelRow[nIterator] = moRGBBlack
-    moPixelRow.show()
+                    moSettingRow[nIterator] = moRGBBlack
+    moSettingRow.show()
 
 def WarningShotMode():
-    moPixelRow.fill(moRGBBlack)
-    moPixelRow[0] = (255, 64, 64)
-    moPixelRow.show()
+    moSettingRow.fill(moRGBBlack)
+    moSettingRow[0] = (255, 64, 64)
+    moSettingRow.show()
 
 def DisableWarningShotMode():
-    moPixelRow.fill(moRGBBlack)
-    moPixelRow.show()
+    moSettingRow.fill(moRGBBlack)
+    moSettingRow.show()
 
 def OVERLOADMODE():
     pass
@@ -163,6 +172,7 @@ btn2 = Debouncer(ButtonRead(BTN_RIGHT))
 # btnTrigger = Debouncer(ButtonRead(BTN_TRIGGER))
 btn1Down = 0
 btn2Down = 0
+WarningShotMode()
 
 while True:
     btn1.update()
@@ -184,15 +194,18 @@ while True:
             DisableWarningShotMode()
             DisableOverload()
             # decrement setting if under 2 sec press
-            moAudioPlay.play(moSettingSound)
-            while moAudioPlay.playing:
-                pass
-            # cp.play_file("adjust.wav")
+            # moAudioPlay.play(moSettingSound)
+            # while moAudioPlay.playing:
+            #   pass
+            moI2SAudio.play(moSettingSound)
+            # while moI2SAudio.playing():
+            #    pass
             SettingDecrease(1)
             # print(mnIntensitySetting)
         else:
-            if mnIntensitySetting == 0:
-                WarningShotMode()
+            pass
+            # if mnIntensitySetting == 0:
+            #    WarningShotMode()
 
     if btn2.fell:
         btn2Down = time.monotonic()
@@ -205,12 +218,14 @@ while True:
             # decrement setting if under 2 sec press
             # Plays the sample once when loop=False,
             # and continuously when loop=True. Does not block
-            moAudioPlay.play(moSettingSound)
-            while moAudioPlay.playing:
-                pass
-            # cp.play_file("adjust.wav")
+            # moAudioPlay.play(moSettingSound)
+            # while moAudioPlay.playing:
+            #    pass
+            moI2SAudio.play(moSettingSound)
+            # while moI2SAudio.playing:
+            #    pass
             SettingIncrease(1)
-            # print(mnIntensitySetting)
+            print(mnIntensitySetting)
         else:
             if mnIntensitySetting == 15:
                 OVERLOADMODE()
